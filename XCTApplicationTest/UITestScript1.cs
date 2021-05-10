cc7307ad0b073a752dab614654e2df1e38be0c5c
kenny_dizi <chitrung09t2@gmail.com>
2021-05-11 03:35:06 +0700

    add simple new diary page UI test

--- XCTApplicationTest/UITestScript1.cs
+++ XCTApplicationTest/UITestScript1.cs
@@ -24,5 +24,22 @@ namespace XCTApplicationTest
             Tap("ButtonLoginAId");
             ShouldSee("Syncfusion License");
         }
+
+        [Test]
+        public void TestComboboxSource()
+        {
+            Input("EntryUserAId", "test user");
+            Input("EntryPasswordAId", "mysecret");
+            Tap("ButtonLoginAId");
+
+            After(seconds: 7);
+
+            // go back at the first time because syncfusion license dialog, we need press back button to close it
+            GoBack();
+
+            // 
+            Tap("ButtonAddPhotoAId");
+            ShouldSee("Pick image from camera");
+        }
     }
 }
