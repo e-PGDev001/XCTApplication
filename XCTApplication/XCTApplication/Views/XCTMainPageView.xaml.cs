cb03989c4f8a916f752f983d3dba8705bc3f11f0
kenny_dizi <chitrung09t2@gmail.com>
2021-05-11 03:04:58 +0700

    reactive login page

--- XCTApplication/XCTApplication/Views/XCTMainPageView.xaml.cs
+++ XCTApplication/XCTApplication/Views/XCTMainPageView.xaml.cs
@@ -1,10 +1,36 @@
-﻿namespace XCTApplication.Views
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
+using System;
+
+namespace XCTApplication.Views
 {
     public partial class XCTMainPageView
     {
+        private readonly CompositeDisposable _compositeDisposable;
+
         public XCTMainPageView()
         {
             InitializeComponent();
+            _compositeDisposable = new CompositeDisposable();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.WhenAnyValue(view => view.EntryPassword.Text,
+                    view => view.EntryUser.Text)
+                .Subscribe(values =>
+                {
+                    this.ButtonLogin.IsEnabled =
+                        !string.IsNullOrEmpty(values.Item1) && !string.IsNullOrEmpty(values.Item2);
+                })
+                .DisposeWith(_compositeDisposable);
+        }
+
+        protected override void OnDisappearing()
+        {
+            _compositeDisposable.Dispose();
+            base.OnDisappearing();
         }
     }
 }
