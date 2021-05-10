2ee410a4effbc0267e3c61400cc216022d89258c
kenny_dizi <chitrung09t2@gmail.com>
2021-05-11 03:05:19 +0700

    add home page UI test

---
+++ XCTApplicationTest/UITestScript1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using QuickTest;
+using XCTApplication;
+
+namespace XCTApplicationTest
+{
+
+    public class UITestScript1 : QuickTest<App>
+    {
+        [SetUp]
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+
+            Launch(new App());
+        }
+
+        [Test]
+        public void LoginDiaryApp()
+        {
+            Input("EntryUserAId", "test user");
+            Input("EntryPasswordAId", "mysecret");
+            Tap("ButtonLoginAId");
+            ShouldSee("Syncfusion License");
+        }
+    }
+}
