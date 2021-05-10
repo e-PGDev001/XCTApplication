06cfb064d8d67e46caa5f29057a698c6f6b2438c
kenny_dizi <chitrung09t2@gmail.com>
2021-05-10 14:28:52 +0700

    add application class

---
+++ XCTApplication/XCTApplication.Android/MainApplication.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Runtime;
+using System;
+using Plugin.CurrentActivity;
+using AApplication = Android.App.Application;
+
+namespace XCTApplication.Droid
+{
+#if DEBUG
+    [Application(Debuggable = true)]
+#else
+[Application(Debuggable = false)]
+#endif
+    public class MainApplication : AApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer)
+            : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            CrossCurrentActivity.Current.Init(this);
+        }
+    }
+}
